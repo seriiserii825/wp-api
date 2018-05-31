539051943f03eefffa3bd9c602724f76d57ab1ec
serii <seriiburduja@gmail.com>
2018-05-31 20:53:32 +0300

    первый ajax запрос

--- wp-content/plugins/wfm-ajax/wfm-ajax-scripts.js
+++ wp-content/plugins/wfm-ajax/wfm-ajax-scripts.js
@@ -1,3 +1,24 @@
 jQuery(document).ready(function($) {
-    alert(111);
+
+    $('#wfm_ajax_form').on('submit', function(){
+
+        $.ajax({
+            url: ajaxurl,
+            type: 'POST',
+            data: {
+                data: $('#wfm_theme_option_bg_id').val(),
+                action: 'wfm_ajax_url'
+            },
+        })
+        .done(function(res) {
+            alert(res);
+        })
+        .fail(function(res) {
+            alert("error");
+        })
+
+        return false;
+    });
+
+    
 });
--- wp-content/plugins/wfm-ajax/wfm-ajax.php
+++ wp-content/plugins/wfm-ajax/wfm-ajax.php
@@ -5,7 +5,20 @@
 
 add_action( 'admin_menu', 'wfm_admin_menu' );
 add_action( 'admin_init', 'wfm_admin_settings' );
+add_action( 'wp_ajax_wfm_ajax_url', 'wfm_ajax_data' );
 
+function wfm_ajax_data(){
+    if(isset($_POST['data'])){
+        update_option( 'wfm_theme_option_name', $_POST['data'] );
+        
+        wp_die();
+    }
+}
+
+/**
+ * Создаем секцию и поле в админке
+ * @return [type] [description]
+ */
 function wfm_admin_settings(){
     register_setting( 'wfm_theme_options_grop', 'wfm_theme_option_name' );
 
@@ -14,6 +27,10 @@ function wfm_admin_settings(){
     add_settings_field( 'wfm_theme_option_bg_id', 'Цвет body', 'wfm_theme_option_bg_cb', 'wfm-theme-options-page', 'wfm_theme_options_section', ['label_for' => 'wfm_theme_option_bg_id'] );
 }
 
+/**
+ * Создаем инпут в админке
+ * @return [type] [description]
+ */
 function wfm_theme_option_bg_cb(){
     $option = get_option( 'wfm_theme_option_name' );
     ?>
@@ -21,21 +38,33 @@ function wfm_theme_option_bg_cb(){
     <?php
 } 
 
+/**
+ * Создаем страницу в блоке настроек и подключаем скрипт
+ * @return [type] [description]
+ */
 function wfm_admin_menu(){
     $suffix_hook = add_options_page( 'menu_options', 'Опции ajax', 'manage_options', 'wfm-theme-options-page', 'wmf_options_cb' );
 
     add_action( 'admin_print_scripts-' . $suffix_hook, 'wfm_admin_scripts' );
 }
 
+/**
+ * Подключаем скрипт
+ * @return [type] [description]
+ */
 function wfm_admin_scripts(){
     wp_enqueue_script( 'wfm_ajax_admin_scripts', plugins_url( 'wfm-ajax-scripts.js', __FILE__ ), ['jquery'], null, true );
 }
 
+/**
+ * Вывод html элементов в админке
+ * @return [type] [description]
+ */
 function wmf_options_cb(){
     ?>
         <div class="wrap">
             <h2>Опции меню</h2>
-            <form action="options.php" method="post">
+            <form action="options.php" method="post" id="wfm_ajax_form">
                 <?php settings_fields( 'wfm_theme_options_grop' ); ?>
                 <?php do_settings_sections( 'wfm-theme-options-page' ); ?>
 
